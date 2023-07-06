6cdebe605b4ad45b579134067fd448eccaccf4f1
Abdullah <abdallah0115019@gmail.com>
2023-07-06 14:00:30 +0300

    first step tanent

---
+++ components/CommercialComponents/Step_One.tsx
@@ -0,0 +1,43 @@
+import {Form} from 'react-bootstrap';
+import { ChangeEvent , useState } from 'react';
+import moment from 'moment-hijri';
+
+function Step_One() {
+  const [gregorianDate, setGregorianDate] = useState('');
+  const handleDateChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const selectedDate = event.target.value;
+    const hijriDate = moment(selectedDate, 'YYYY-MM-DD').format('iYYYY/iM/iD');
+    setGregorianDate(selectedDate);
+    console.log(hijriDate);
+  };
+  
+  return (
+    <>
+    <h3 className='text-center fw-bold'>معلومات المالك</h3>
+    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Control type="number" className='text-center' placeholder="رقم هوية المالك" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Control type="text" className='text-center' placeholder="اسم المالك الثالثي" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+        <Form.Control type="tel" className='text-center' placeholder="رقم جوال المالك في أبشر05xxxxx" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
+        <Form.Control type="date" className='text-center' value={gregorianDate} onChange={handleDateChange}  placeholder="تاريخ ميلاد المالك" />
+        {gregorianDate && (
+        <p>Hijri Date: {moment(gregorianDate).format('iYYYY/iM/iD')}</p>
+      )}
+      </Form.Group>
+      <h3 className='text-center fw-bold'>العنوان الوطني للمالك</h3>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Control type="text" className='text-center' placeholder="المدينة" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Control type="text" className='text-center' placeholder="الحي" />
+      </Form.Group>
+    </>
+  )
+}
+
+export default Step_One
--- components/ResidentialComponents/Resid_Tanent_Form.tsx
+++ components/ResidentialComponents/Resid_Tanent_Form.tsx
@@ -1,9 +1,57 @@
-
+import { useMultistepsForm } from "@/useMultiForm";
+import { FormEvent } from "react";
+import { Button, Form ,InputGroup } from "react-bootstrap";
+import Step_One from "../CommercialComponents/Step_One";
+import Step_Two from "./Form_Steps/Step_Two";
+import Step_Three from "./Form_Steps/Step_Three";
+import Step_Four from "./Form_Steps/Step_Four";
+import Step_Five from "./Form_Steps/Step_Five";
+import Step_Six from "./Form_Steps/Step_Six";
 function Resid_Tanent_Form() {
+    const {next,back,step,steps,currentStepIndex,isFirstStep,isLastStep,goTo} = useMultistepsForm([<Step_One key="0"/>,<Step_Two key='1'/>,<Step_Three key="2"/>,<Step_Four key="3"/>,<Step_Five key="4"/>,<Step_Six key="4"/>]);
+    const postData = async () => {
+      const url = 'https://stage.al3gd.com/order/check-nid'; // Replace with your API endpoint URL
+    
+      const data = {
+        nid: '1126474327', // Replace with the actual national ID
+        bdate: '1420-07-25', // Replace with the actual birth date
+        _token: '(aTzF6tMJaWNg57ELZrm14EXnHsHKZyFWnh8pf9uX', // Replace with the actual token
+      };
+    
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+      };
+    
+      try {
+        const response = await fetch(url, requestOptions);
+        const result = await response.json();
+        console.log(result); // Do something with the response
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+    
+   
+    
+    function onSubmit(e: FormEvent) {
+        e.preventDefault()
+        if(isFirstStep){
+          postData();
+        }
+        if (!isLastStep) return next()
+        alert("dddddddd");
+        
+      }
+      
   return (
-    <div>
-      Resid_Tanent_Form
-    </div>
+    <Form onSubmit={onSubmit}>
+        <p>`${steps.length}`/`${currentStepIndex+1}`</p>
+        {step}
+        {<Button variant="primary" className='btnGreen' type="submit" >{isLastStep ? "ارسال" : "التالي"}</Button>}
+        {!isFirstStep&&<Button variant="primary" className='btnGreen' onClick={back}>رجوع</Button>}
+    </Form>
   )
 }
 
